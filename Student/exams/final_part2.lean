dca596b4307cb07f024c0b4cd8643c72f91843e0
Ryland Birchmeier <rylandbirchmeier@gmail.com>
2023-12-05 19:40:57 +0000

    still working on problem 4 part 2 the second part. hmmmmmmmmmmmmm it can't be that hard...

--- Student/exams/final_part2.lean
+++ Student/exams/final_part2.lean
@@ -14,9 +14,12 @@ existentially quantified propositions.
 Use set comprehension notation in Lean to define *odds*
 as the set of odd numbers, by way of a membership predicate
 for this set.
+
+[[ Ryland: Done ]]
 -/
 
 -- Here
+def odds : Set Nat := { n : Nat | n % 2 = 1}
 
 /-!
 ## Problem #2:
@@ -26,18 +29,25 @@ define the set, *perfect_squares*, of natural numbers, n,
 such that each n is the square of some natural number, m.
 For example, 36 is a perfect square because it is the square
 of another number, namely m = 6.
+
+[[ Ryland: Done ]]
 -/
 
 -- Here
+def perfect_squares : Set Nat := { n : Nat | ∃ (m : Nat), m^2 = n}
+#check perfect_squares
 
 /-!
 ## Problem #3:
 
 Use set comprehension notation to define the set, odd_perfects,
 to be the intersection of the odds and the perfect squares.
+
+[[ Ryland: Done ]]
 -/
 
 -- Here
+def odd_perfects : Set Nat := odds ∩ perfect_squares
 
 /-!
 ## Problem #4:
@@ -47,3 +57,4 @@ Hint: A proof within a proof.
 -/
 
 -- Here
+def nine_in_odd_perfects : 9 ∈ odd_perfects := ⟨ rfl, _ ⟩
